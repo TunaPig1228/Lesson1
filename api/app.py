e73d39cff3d5063a7cea84bc95fe63c7037a0627
TunaPig1228 <132420858+TunaPig1228@users.noreply.github.com>
2024-02-11 07:13:19 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request
+import google.generativeai as genai
 
 app = Flask(__name__)
 
-@app.route('/',methods=["GET"])
+@app.route('/',methods=["POST"])
 def query():
-    return "success"
+    # 从请求中获取 JSON 数据
+    query = request.get_json()['query']
+    genai.configure(api_key = 'AIzaSyCv7-MKV9NqO6DqRXYu4GXysWeSnGBEijk')
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(query)
+    # 处理接收到的数据（这里简单地将数据返回）
+    return jsonify(response.text)
