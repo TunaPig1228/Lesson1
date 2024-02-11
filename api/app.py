38a35e9d8fad8d2a9582d2cfed5c9e21d264c541
TunaPig1228 <132420858+TunaPig1228@users.noreply.github.com>
2024-02-11 10:31:45 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -55,6 +55,7 @@ def query():
     # 从请求中获取 JSON 数据
     query = request.form.get('query')
     query = find_and_convert_numbers(query)
+    print(query)
     genai.configure(api_key = 'AIzaSyCv7-MKV9NqO6DqRXYu4GXysWeSnGBEijk')
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(query)
